b03ffa7e31fbad18289938f500ea0b9b74b304fc
Supun Gamlath <supungamlath@outlook.com>
2023-01-04 17:14:55 +0530

    Fixed schema

--- src/schemas/onlineLoanSchema.js
+++ src/schemas/onlineLoanSchema.js
@@ -1,18 +1,19 @@
 import * as yup from 'yup'
 
 export const onlineLoanSchema = yup.object({
-  fixedDepositID: yup.string()
-    .min(10, 'Minimum  characters allowed are 11')
-    .max(10, 'Maximum characters allowed are 11')
-    .required('Account number is required'),
+  // fixedDepositID: yup.string()
+  //   .min(10, 'Minimum  characters allowed are 11')
+  //   .max(10, 'Maximum characters allowed are 11')
+  //   .required('Account number is required'),
   amount: yup.number()
     .positive('Enter a vaild amount')
     .required('Amount is required'),
   period: yup.number()
-    .positive('Enter a vaild amount')
-    .required('Amount is required'),
-  linkedAccountID: yup.string()
-    .min(10, 'Minimum  characters allowed are 11')
-    .max(10, 'Maximum characters allowed are 11')
-    .required('Account number is required'),
+    .positive('Enter a vaild time period')
+    .integer('Time period cannot be a decimal')
+    .required('Time period is required'),
+  // linkedAccountID: yup.string()
+  //   .min(10, 'Minimum  characters allowed are 11')
+  //   .max(10, 'Maximum characters allowed are 11')
+  //   .required('Account number is required'),
 })
--- src/schemas/transferSchema.js
+++ src/schemas/transferSchema.js
@@ -1,15 +1,20 @@
 import * as yup from 'yup'
 
 export const transferSchema = yup.object({
-  fromAccountID: yup.string()
-    .min(10, 'Minimum  characters allowed are 11')
-    .max(10, 'Maximum characters allowed are 11')
-    .required('Account number is required'),
+  // fromAccountID: yup.string()
+  //   .min(10, 'Minimum  characters allowed are 11')
+  //   .max(10, 'Maximum characters allowed are 11')
+  //   .required('Account number is required'),
   amount: yup.number()
     .positive('Enter a vaild amount')
     .required('Amount is required'),
   toAccountID: yup.string()
-    .min(10, 'Minimum  characters allowed are 11')
-    .max(10, 'Maximum characters allowed are 11')
+    .min(5, 'Invalid Account Number')
+    .max(10, 'Invalid Account Number')
+    .matches(/^[0-9]+$/, "Invalid Account Number")
     .required('Account number is required'),
+  remarks: yup.string()
+    .min(3, 'At least 3 characters required')
+    .max(20, 'Characters limit exceeded')
+    .required('Remarks are required'),
 })
