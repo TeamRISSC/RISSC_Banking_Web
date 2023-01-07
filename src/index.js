6f153907f2317cbcb9b6b54da5112377fb890d87
Supun Gamlath <supungamlath@outlook.com>
2023-01-07 18:49:08 +0530

    Fix server app name

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import axios from "axios";
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
 import {QueryClientProvider, QueryClient} from 'react-query'
@@ -10,6 +11,8 @@ import { ThemeProvider } from '@mui/material';
 import { theme } from './theme';
 
 const queryClient = new QueryClient()
+const pkg = require("../package.json");
+axios.defaults.baseURL = process.env.PORT ? pkg.remote : pkg.localhost;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
